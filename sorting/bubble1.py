e10f4473341bb3656be3f46f41dc30617176dc7e
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2022-03-22 17:09:48 +0200

    commit bubble1

---
+++ sorting/bubble1.py
@@ -0,0 +1,6 @@
+def bubbleSort1(myList: list) -> list:
+    for i in range(len(myList) - 1):
+        for j in range(len(myList) - i - 1):
+            if myList[j] > myList[j + 1]:
+                myList[j], myList[j + 1] = myList[j + 1], myList[j]
+    return myList
